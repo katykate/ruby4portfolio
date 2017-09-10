01487a590e41a2476d7c06930d09b89e05ee195c
Katy Kate <krawczykate@gmail.com>
2017-09-10 16:19:41 +0100

    more changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,23 +14,17 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
-  # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
-  # `config/secrets.yml.key`.
-  config.read_encrypted_secrets = true
-
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
   # config.assets.css_compressor = :sass
-  config.serve_static_assets = true
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
-  config.assets.digest = true
+  config.assets.compile = false
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
@@ -60,7 +54,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "ruby4portfolio_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "cf_shop_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
@@ -84,9 +78,13 @@ Rails.application.configure do
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # ActionCable setup for Heroku
+  config.web_socket_server_url = "wss://quiet-lake-66250.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://quiet-lake-66250.herokuapp.com', 'http://quiet-lake-66250.herokuapp.com']
 end
